b9e978296722ff81834d1781c4d738cb70cab2f8
rajagogu <91778408+rajagogu@users.noreply.github.com>
2021-11-17 12:47:53 +0530

    Added Missing Files

---
+++ src/app/companyModel.ts
@@ -0,0 +1,18 @@
+export class CompanyModel {
+    id: number;
+    name: string;
+    brewery_type: string;
+    street: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country: string;
+    longitude: string;
+    latitude: string;
+    phone: string;
+    website_url: string;
+    updated_at: string;
+    created_at: string
+
+
+}
